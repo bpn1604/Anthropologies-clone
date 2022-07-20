539c6829c41e3222557465d2951f8345b059e1d5
bpn1604 <bipin.kumar2015@vitalum.ac.in>
2022-07-20 23:19:40 +0530

    navbar check

--- project/src/App.js
+++ project/src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <Footer />
     </div>
--- project/src/index.js
+++ project/src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+  // <React.StrictMode>
+  <ChakraProvider>
+  <Provider store={store}>
+  <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    </Provider>
     </ChakraProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
